138cb566babf1947ea2bce7450d4667b5ae97fdb
Felix Leboo <felixlemi@gmail.com>
2018-09-29 17:05:05 -0500

    Implemented shelfChangeHandler function

--- src/containers/BooksContent.js
+++ src/containers/BooksContent.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
+import { getAll, update } from '../BooksAPI';
 import Aux from '../hoc/Aux/Aux';
 import Bookshelves from '../components/Bookshelves/Bookshelves';
 
 class BooksContent extends Component {
   state = {
     books: []
+  }
+
+  // Function to handle changing books in shelves
+  shelfChangeHandler = (event, bookUpdate) => {
+    const shelf = event.target.value;
+
+    update(bookUpdate, shelf).then((res) => {
+      console.log('[UPDATE Response]', res);
+
+      bookUpdate.shelf = shelf;
+      let updatedBooks = this.state.books.filter((book) => book.id !== bookUpdate.id)
+      updatedBooks.push(bookUpdate);
+      
+      this.setState({books: updatedBooks});
+    })
   }
 
   render() {
